12147f4e13768ef7594b294752c3002e0a788fef
Emily Tang <emitang@mit.edu>
2014-10-02 13:41:30 -0400

    some changes to the css to make the website look nicer and some fixes to the index page

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>Emily Tang's Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="p1.css">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<div id="sidebar">
+		<img src="selfpic.jpg" alt="picture of me">
+		<h1 id="titulo">Emily Tang's Projects</h1>
+		<ul id="pbar">
+			<li><p class="plist">p2</p><a href = "/">view</a> // <a href = "/">github</a></li><br>
+			<li><p class="plist">p3</p><a href = "/">view</a> // <a href = "/">github</a></li><br>
+			<li><p class="plist">p4</p><a href = "/">view</a> // <a href = "/">github</a></li><br>
+		</ul>
+	</div>
+
+	<div id="page-content">
+		<h1 class="contenthead">Description</h1>
+		<p class="contentp">My name is Emily Tang and I'm approximately a second-year MIT student. I'm looking to major in Computer Science and either minor or also major in Linguistics. I was born in Virginia, but I've lived in Plano, Texas (a suburb of Dallas) for most of my life.</p>
+		<h1 class="contenthead">Course</h1>
+		<p class="contentp">I'm taking this course because I've studied lots of front-end coding for web development, but I haven't ever really looked closely at back-end, so I'm interested in learning more.</p>
+		<h1 class="contenthead">Programming</h1>
+		<p class="contentp">My previous programming experience includes knowledge of Java, some of the basics of Python, and experience with mainly front-end web development</p>
+		<h1 class="contenthead">Platform</h1>
+		<p class="contentp">I'm working on a Macbook Pro 13" from 2012 running OSX Mavericks 10.9.2</p>
+		<h1 class="contenthead">Other</h1>
+		<p class="contentp" id="contentbottom">I'm currently on a medical leave from MIT and taking some classes to get back on track for readmission.</p>
+	</div>
+</body>
+</html>
